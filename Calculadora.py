9584c371bda9b9c755175fa480954c57fda1be3a
= <=>
2023-04-12 16:31:57 -0500

    Esta version prueba menu y suma

--- Calculadora.py
+++ Calculadora.py
@@ -2,23 +2,31 @@
 #   Crear una aplicacion que implemente: suma, resta, multiplicacion y division de n numeros.
 #   Validar los numeros por parametros para los numeros
 
-######  Principal   ######
-
-print("Sumas, resta, multiplicacion y division de n numeros:")   #Titulo del programa
-
 def Menu():
     print("1. Suma")
     print("2. Resta")
     print("3. Multiplicacion")
     print("4. Division")
-    Opc = int(input("Ingrese opcion: \n"))
+    print("5. Salir")
+    Opc = int(input(f"Ingrese opcion: \n"))
     return Opc
+
+def Suma(n):
+    SumaTotal = 0
+    for i in range(0,n):
+        SumaTotal = SumaTotal + int(input(f"Ingrese {i+1} numero: \n"))
+    return SumaTotal
     
+######  Principal   ######
+print("Sumas, resta, multiplicacion y division de n numeros:")   #Titulo del programa
 
 #validacion
-n = int(input("Ingrese n: "))    #Ingreso de la cantidad de numeros a operar
-Operacion = Menu()
 
+n = int(input("Ingrese cantidad de numeros a operar: "))    #Ingreso de la cantidad de numeros a operar
+Operacion = Menu()
+while Operacion < 5 and Operacion > 0:
+    if Operacion == 1:
+        print(f"La suma es: {Suma(n)}")
 
 
 

94aac8ac2bb2f6d917e5ad6cf358f50e51307ef9
= <=>
2023-04-12 16:11:46 -0500

    Version de prueba para menu

--- Calculadora.py
+++ Calculadora.py
@@ -11,7 +11,7 @@ def Menu():
     print("2. Resta")
     print("3. Multiplicacion")
     print("4. Division")
-    opcion = int(print("Ingrese opcion: \n"))
+    Opcion = int(print("Ingrese opcion: \n"))
     return opcion
     
 
@@ -31,67 +31,3 @@ Operacion = Menu()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def suma_numeros(numeros):
-    return sum(numeros)
-
-def resta_numeros(numeros):
-    resultado = numeros[0]
-    for i in range(1, len(numeros)):
-        resultado -= numeros[i]
-    return resultado
-
-def multiplicacion_numeros(numeros):
-    resultado = 1
-    for num in numeros:
-        resultado *= num
-    return resultado
-
-def division_numeros(numeros):
-    resultado = numeros[0]
-    for i in range(1, len(numeros)):
-        if numeros[i] == 0:
-            print("Error: No se puede dividir por cero")
-            return None
-        resultado /= numeros[i]
-    return resultado
-
-operaciones = {
-    "1": suma_numeros,
-    "2": resta_numeros,
-    "3": multiplicacion_numeros,
-    "4": division_numeros
-}
-
-while True:
-    print("\nIngrese una lista de números separados por comas (ejemplo: 1,2,3):")
-    numeros = input().split(",")
-    numeros = [float(num) for num in numeros]
-    print("\nSeleccione la operación que desea realizar:")
-    print("1. Suma")
-    print("2. Resta")
-    print("3. Multiplicación")
-    print("4. División")
-    operacion = input()
-    resultado = operaciones.get(operacion)(numeros)
-    if resultado is not None:
-        print("\nEl resultado de la operación es:", resultado)
-    print("\n¿Desea realizar otra operación? (s/n)")
-    continuar = input()
-    if continuar.lower() != "s":
-        break
